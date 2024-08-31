61e5d9b684ddf027b594d196b9ce8a662412b822
Ray <rey@gmail.com>
2024-08-31 22:08:13 +0330

    tagier average

---
+++ jam.py
@@ -0,0 +1,9 @@
+a = int(input('Enter first integer number: '))
+b = int(input('Enter second integer number: '))
+c=int(input('Enter third integer number:'))
+sum1= a+b+c
+print(sum1)
+sum2=a*b*c
+print(sum2)
+sum3=(a+b+c)/3
+print(sum3)
---
+++ mmm.py
@@ -0,0 +1,8 @@
+def average(n):
+    num = 0 
+    for num_2 in args:
+        num += num_2
+    
+ x=num/len(n)
+
+print(x)
